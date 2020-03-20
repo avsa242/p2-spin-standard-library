5946cabeb9e20a9badbf55ec8433a2762dd2a9d4
Jesse Burt <avsa242@gmail.com>
2020-03-20 15:10:56 -0400

    Bitbanged (faster) SPI engine: - Remove IO pin manipulation methods - these are now provided by fastspin builtins - Remove unused code

--- test.sh
+++ test.sh
@@ -13,12 +13,12 @@ fi
 
 while read line
 do
-    ${SPINC} -2 -L "./library" "$line" >/dev/null
+    ${SPINC} -q -2 -L "./library" "$line" >/dev/null
     if [ $? != 0 ]
     then 
         echo
-        echo "${SPINC} -2 -L ./library $line"
-        ${SPINC} -2 -L ./library "$line"
+        echo "${SPINC} -q -2 -L ./library $line"
+        ${SPINC} -q -2 -L ./library "$line"
         echo
     fi
 done < <(cat MANIFEST|grep spin2)
